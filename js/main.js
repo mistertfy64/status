d85fab85d2ade211aae70a8491b86667221c1587
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-05-29 17:06:22 +0700

    change: lots of things

--- js/main.js
+++ js/main.js
@@ -38,12 +38,13 @@ function getBarColor(data) {
 function getFilledBar(data) {
 	const filledBar = document.createElement("div");
 	filledBar.classList.add("graph-bar__bar");
+	// bar color
 	if (GOOD_STATUS_CODES.indexOf(data.statusCode) === -1) {
 		filledBar.style.background = `repeating-linear-gradient(
     45deg,
     var(--color-bad),
     var(--color-bad) 8px,#cf0000 8px,#cf0000 16px
-)`;
+  )`;
 	} else {
 		filledBar.style.background = `linear-gradient(to top, var(--color-good), var(--color-good) ${
 			100 - (data.timeTotal / GRAPH_LOWER_POINT) * 100
@@ -53,20 +54,95 @@ function getFilledBar(data) {
 			100 - (data.timeTotal / GRAPH_LOWER_POINT) * 100
 		}%, var(--color-transparent) 100%)`;
 	}
+
+	const barInformation = document.createElement("div");
+	barInformation.classList.add("graph-bar__details");
+	barInformation.classList.add("monospace");
+	barInformation.append(getBarUppermostText(data));
+	barInformation.append(getBarUpperText(data));
+	barInformation.append(getBarMiddleText(data));
+	barInformation.append(getBarLowerText(data));
+	barInformation.style.position = "absolute";
+	barInformation.style.display = "none";
+
+	if (GOOD_STATUS_CODES.indexOf(data.statusCode) === -1) {
+		barInformation.classList.add("graph-bar__details--bad");
+	} else {
+		barInformation.classList.add("graph-bar__details--good");
+	}
+	filledBar.appendChild(barInformation);
+	addEventListenersToBar(filledBar);
+
 	return filledBar;
+}
+
+function addEventListenersToBar(filledBar) {
+	filledBar.addEventListener("mouseover", function (event) {
+		const details = Array.from(this.children).filter(function (element) {
+			return (
+				Array.from(element.classList).indexOf("graph-bar__details") > -1
+			);
+		});
+		details[0].style.display = "block";
+	});
+	filledBar.addEventListener("mouseout", function (event) {
+		const details = Array.from(this.children).filter(function (element) {
+			return (
+				Array.from(element.classList).indexOf("graph-bar__details") > -1
+			);
+		});
+		details[0].style.display = "none";
+	});
 }
 
 function getTextBar(data) {
 	const text = document.createElement("div");
 	text.style.textAlign = "center";
 	text.classList.add("monospace");
+	text.style.position = "absolute";
 	text.style.fontSize = "12px";
 	if (GOOD_STATUS_CODES.indexOf(data.statusCode) === -1) {
-		text.innerText = data.statusCode ?? "ERROR";
-	} else {
-		text.innerText = `${Math.round(data.timeTotal * 1000)}ms`;
+		// text.innerText = `ERROR ${data.statusCode}` ?? "ERROR ???";
 	}
 	return text;
+}
+
+function getBarUppermostText(data) {
+	const text = document.createElement("div");
+	text.textContent = `${new Date(data.timestamp).toISOString()}`;
+	text.style.fontSize = "10px";
+	return text;
+}
+
+function getBarUpperText(data) {
+	const text = document.createElement("div");
+	text.textContent = `(${getFormattedTime(data)})`;
+	text.style.fontSize = "8px";
+	return text;
+}
+
+function getBarMiddleText(data) {
+	const text = document.createElement("div");
+	if (GOOD_STATUS_CODES.indexOf(data.statusCode) === -1) {
+		text.textContent = `ERROR`;
+	} else {
+		text.textContent = `${Math.round(data.timeTotal * 1000)}ms`;
+	}
+	text.style.fontSize = "14px";
+	return text;
+}
+
+function getBarLowerText(data) {
+	const text = document.createElement("div");
+	text.textContent = `${data.statusCode}`;
+	text.style.fontSize = "10px";
+	return text;
+}
+
+function getFormattedTime(data) {
+	const time = new Date();
+	const minutes = Math.floor((time - data.timestamp) / (60 * 1000));
+	return `${minutes} min. ago`;
 }
 
 async function getData(id) {
